e1ebdb010c731c8b729a64d407ae7d8abe527e1c
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2023-02-21 22:16:42 -0500

    Commit message: Add files and update notebook-openai.dib for OpenAI sample project

---
+++ openaisample.py
@@ -0,0 +1,11 @@
+import os
+import openai
+openai.api_key = open('pykey.txt').read()
+
+question = "List the 3 most populated US states with population, and abbreviation, output in json format"
+aimodel = "text-davinci-003"
+
+os.system('cls')
+print('Submitting request to openai...')
+response = openai.Completion.create(model=aimodel,prompt=question,temperature=0,max_tokens=1000)
+print(response.choices[0].text)
